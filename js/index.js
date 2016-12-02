96851b590d8fd4b876ba668de3015449a6904d4b
ManningMorning <zhangmanning2015@gmail.com>
2016-12-02 19:57:23 +0100

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,15 +1,5 @@
 //-------------------functions of buttons --------------------//
 
-//-------------the function of the first button: NEXT
-function toSelect(){
-
-  var inst=document.getElementById("instruction");
-  inst.style.display="none";
-
-  var traget=document.getElementById("selectPage");
-  traget.style.display="block";
- }
-
 //---------variable color is set to store the selected color by user and transfor to the drawing on the canvas
   var color;
   var colorList = [];
@@ -28,9 +18,9 @@ function toSelect(){
      var aColor = obj.options[i].value;
      if(aColor != color){
         list.push(aColor);
+   }
   }
- }
- }
+}
 
 
   //---------------------the function of the second button: GO
@@ -40,18 +30,18 @@ function toSelect(){
 
  //this if statement will inform the user to choose a color if they haven't
     if(color == "none"){
-         alert("choose a color~");
+         alert("Oh, you forget to choose a color.");
     } else {
       // display the game canvas to the website
       var c=document.getElementById("canvasPlace");
       c.style.display="block";
     //then remove the button
-     var traget=document.getElementById("selectPage");
-     traget.style.display="none";
+    var inst=document.getElementById("instruction");
+    inst.style.display="none";
 
      allColor('selectC',colorList);
-     alert(colorList);
      draw();
+     interval = setInterval("draw()", time);
    }
  }
 
@@ -62,7 +52,7 @@ function toSelect(){
     var c=document.getElementById("canvasPlace");
     c.style.display="none";
 
-    var traget=document.getElementById("selectPage");
+    var traget=document.getElementById("instruction");
     traget.style.display="block";
 
     //restart means number of caughtCir should back to 0 and life back to 5
@@ -94,6 +84,7 @@ function toSelect(){
 
  // draw the circle so it appears onload
  function draw () {
+
 
  canvas.width = window.innerWidth*0.9;
  canvas.height = window.innerHeight*0.8;
@@ -134,14 +125,60 @@ function toSelect(){
     }
  }
 
- function level(){
-   if(caughtCir>=10){
-     numOfCir = 3;
-   }else if(caughtCir >= 20){
-     numOfCir = 5;
-   }
+//function to add text on canvas:
+//indicating how many circles are caught and how many lives left
+ function writeMessage(canvas) {
+     context.clearRect(0, 0, canvas.width, 0.1*canvas.height);
+     context.font = "24px  'Architects Daughter'";
+     context.textAlign = "left";
+     context.textBaseline = "top";
+     context.fillStyle = 'white';
+     //drawHeart(life);
+     context.fillText("○ caught: " + caughtCir + "        ||        ♥ left： " + drawHeart(life)  , 0.4*canvas.width, 25);
+ }
 
+
+ function drawHeart(num){
+ var hearts = [];
+   for(i=0; i<num; i++){
+     hearts.push(' ♥ ');
+   }
+   return hearts;
+ }
+
+var time =3000;
+var interval;
+
+//function to increase the hardness of the game
+//the more circles are caught, the more and faster there will be:
+ function level(){
+   switch(caughtCir){
+     case 10:
+       numOfCir = 3;
+       time = 2300;
+       break;
+     case 20:
+       numOfCir = 5;
+       time = 1800;
+       break;
+     case 30:
+       numOfCir = 7;
+       time = 1200;
+       break;
+    case 40:
+       numOfCir = 12;
+       time = 700;
+       break;
+    case 40:
+       numOfCir = 16;
+       time = 350;
+       break;
+   }
 }
+
+
+
+
  // this method does not use "expensive" Math.sqrt
  function mouseIsInsideCircle(mouseX, mouseY, cx, cy, radius) {
      var dx = mouseX - cx;
@@ -159,24 +196,7 @@ function toSelect(){
  }
 
 
- //canvas.addEventListener('mousemove', function (evt) {
-//     var mousePos = getMousePos(canvas, evt);
-//     var message = 'Mouse position: ' + parseInt(mousePos.x) + ' , ' + parseInt(mousePos.y);
-//     writeMessage(canvas, message);
-
-// }, false);
-
-
  var mousePos;
- function writeMessage(canvas, message) {
-     context.clearRect(0, 0, canvas.width, 0.1*canvas.height);
-     context.font = "24px  sans-serif";
-     context.textAlign = "left";
-     context.textBaseline = "top";
-     context.fillStyle = 'white';
-     context.fillText(message, 0.5*canvas.width, 25);
-     context.fillText("○ caught: " + caughtCir + " life left： "+life, 0.1*canvas.width, 25);
- }
 
 
  canvas.addEventListener('mousedown', function (evt) {
@@ -189,181 +209,25 @@ function toSelect(){
 
      // if the circle is clicked
      if (mouseIsInsideCircle(mouseX, mouseY, cx, cy, radius)) {
+       clearInterval(interval);
          // erase the canvas
          context.clearRect(0, 30, canvas.width, canvas.height);
          caughtCir ++;
          level();
          draw ();
+         interval = setInterval("draw()",time);
      } else {  //if the play didn't clicked on the circle
        context.clearRect(0, 30, canvas.width, canvas.height);
        life --;
        if(life ==0){
-       alert('game over!');
-       backSelect();
-        }
+
+         alert('game over!' + ' You caught ' + caughtCir + ' ○ !');
+         backSelect();
+     }
        else {
-       draw ();
-     }
-
-     }
- }, false);
-
-     traget.style.display="none";
-
-     allColor('selectC',colorList);
-     //alert(colorList);
-     draw();
-     writeMessage(canvas, "");
-   }
- }
-
-
-//-----------------------------------function for RESTART button
-  function backSelect(){
-
-    var c=document.getElementById("canvasPlace");
-    c.style.display="none";
-
-    var traget=document.getElementById("selectPage");
-    traget.style.display="block";
-
-    //restart means number of caughtCir should back to 0 and life back to 5
-    caughtCir=0;
-    life = 5;
-
-    colorList = [];
-  }
-
-
-
-//---------------------functions of canvas and its elements--------------------------------
-
-
-    //a set of important varables and their setting
-    var canvas = document.getElementById('canvasContainer');
-    var context = canvas.getContext('2d');
-
-   // define the circle
-    var radius;
-    var cx;
-    var cy;
-    var numOfCir = 1;
-
-    var caughtCir = 0;
-    var life = 5;
-
-
-
- // draw the circle so it appears onload
- function draw () {
-
- canvas.width = window.innerWidth*0.9;
- canvas.height = window.innerHeight*0.8;
-
- //drawing one with the right color:
- radius = 30 + Math.random()*25;
- cx = radius + (Math.random() * (canvas.width - 2*radius));
- cy = 0.1*canvas.height + radius + (Math.random() * (0.9*canvas.height - 2*radius));
- context.beginPath();//making sure here will only display the new drawing
- context.arc(cx, cy, radius, 0, 2 * Math.PI);
- context.fillStyle = 'black';
- context.fill();
- context.strokeStyle = color;
- context.stroke();
-
- wrongCir();
-
-
- }
-
-
- function wrongCir(){
-
-    for(i=1; i<=numOfCir; i++){
-    var r = 30 + Math.random()*25;
-    var wx = radius + (Math.random() * (canvas.width - 2*radius));
-    var wy = 0.1*canvas.height + radius + (Math.random() * (0.9*canvas.height - 2*radius));
-    context.beginPath();//making sure here will only display the new drawing
-    context.arc(wx, wy, r, 0, 2 * Math.PI);
-    context.fillStyle = 'black';
-    context.fill();
-
-    var num = Math.floor(Math.random() * (colorList.length));
-    context.strokeStyle = colorList[num];
-    context.stroke();
-    }
- }
-
- function level(){
-   if(caughtCir>=10){
-     numOfCir = 3;
-   }else if(caughtCir >= 20){
-     numOfCir = 5;
-   }
-
-}
- // this method does not use "expensive" Math.sqrt
- function mouseIsInsideCircle(mouseX, mouseY, cx, cy, radius) {
-     var dx = mouseX - cx;
-     var dy = mouseY - cy;
-     return (dx * dx + dy * dy <= radius * radius);
- }
-
-
- function getMousePos(canvas, evt) {
-     var rect = canvas.getBoundingClientRect();
-     return {
-         x: evt.clientX - rect.left,
-         y: evt.clientY - rect.top
-     };
- }
-
-
- canvas.addEventListener('mousemove', function (evt) {
-     var mousePos = getMousePos(canvas, evt);
-     var message = 'Mouse position: ' + parseInt(mousePos.x) + ' , ' + parseInt(mousePos.y);
-     writeMessage(canvas, message);
-
- }, false);
-
-
- var mousePos;
- function writeMessage(canvas, message) {
-     context.clearRect(0, 0, canvas.width, 0.1*canvas.height);
-     context.font = "24px  sans-serif";
-     context.textAlign = "left";
-     context.textBaseline = "top";
-     context.fillStyle = 'white';
-     context.fillText(message, 0.5*canvas.width, 25);
-     context.fillText("○ caught: " + caughtCir + " life left： "+life, 0.1*canvas.width, 25);
- }
-
-
- canvas.addEventListener('mousedown', function (evt) {
-
-
-     var mousePos = getMousePos(canvas, evt);
-     var mouseX = mousePos.x;
-     var mouseY = mousePos.y;
-
-
-     // if the circle is clicked
-     if (mouseIsInsideCircle(mouseX, mouseY, cx, cy, radius)) {
-         // erase the canvas
-         context.clearRect(0, 30, canvas.width, canvas.height);
-         caughtCir ++;
-         level();
          draw ();
-     } else {  //if the play didn't clicked on the circle
-       context.clearRect(0, 30, canvas.width, canvas.height);
-       life --;
-       if(life ==0){
-       alert('game over!');
-       backSelect();
-        }
-       else {
-       draw ();
      }
 
      }
+
  }, false);
